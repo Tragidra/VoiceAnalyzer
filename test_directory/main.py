2668abdc79033c235a9192cd7901e743fda14efe
Idris <astralgg@mail.ru>
2023-03-23 11:09:36 +0300

    Добавление инструкций по эксплуатации проекта
    Добавление руководства по использованию проекта,  альтернативной версии нейросети на основе готовой модели Speech-Emotion-Analyzer для маркировки файлов (в процессе работы)

--- test_directory/main.py
+++ test_directory/main.py
@@ -1,34 +1,15 @@
-import librosa
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+#Здесь ведутся работы над использованием https://github.com/MiteshPuthran/Speech-Emotion-Analyzer для большей точности маркировки
+from keras.models import model_from_json
 
-# Load audio files and extract features
-def extract_features(file_path):
-    with open(file_path, "rb") as f:
-        audio, sr = librosa.load(f)
-    mfccs = librosa.feature.mfcc(audio, sr=sr, n_mfcc=40)
-    features = np.mean(mfccs.T, axis=0)
-    return features
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("saved_models/Emotion_Voice_Detection_Model.h5")
+print("Loaded model from disk")
 
-# Load dataset and labels
-angry_files = ["angry1.wav", "angry2.wav", "angry3.wav"]
-calm_files = ["calm1.wav", "calm2.wav", "calm3.wav"]
-X = [extract_features(f) for f in angry_files + calm_files]
-y = [1] * len(angry_files) + [0] * len(calm_files)
-
-# Split dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train SVM classifier
-clf = SVC(kernel="linear")
-clf.fit(X_train, y_train)
-
-# Test classifier on new audio file
-new_file = "test.wav"
-new_features = extract_features(new_file)
-prediction = clf.predict([new_features])[0]
-if prediction == 1:
-    print("The voice in the audio recording is angry.")
-else:
-    print("The voice in the audio recording is calm.")
+# evaluate loaded model on test data
+loaded_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+score = loaded_model.evaluate(x_testcnn, y_test, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
